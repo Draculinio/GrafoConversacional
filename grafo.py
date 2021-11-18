787ac9abf291b614440adfcbf3eb5b6ab7dab46b
Pablo Soifer <psoifer@wwecorp.com>
2021-11-17 23:41:21 -0300

    Dia 10, elementos

--- grafo.py
+++ grafo.py
@@ -1,23 +1,6 @@
 import json
 import random
 
-def guardar_juego(personaje):
-    try:
-        with open('./savegames/savegame.json','w') as f:
-            json.dump(personaje.personaje,f)
-            f.close()
-            print('Juego guardado')
-    except FileNotFoundError:
-        print('Falta el directorio savegame')
-
-def cargar_juego():
-    try:
-        with open('./savegames/savegames.json') as f:
-            print('Juego Cargado')
-            return json.load(f)
-    except:
-        print('No esta el archivo de carga')
-
 def obtener_informacion(archivo):
     try:
         with open('./datos/'+archivo+'.json') as f:
@@ -51,11 +34,29 @@ class Juego:
         elif comando[0] == 'status':
             self.personaje.status()
         elif comando[0] == 'save':
-            guardar_juego(self.personaje)
+            self.guardar_juego()
         elif comando[0] == 'load':
-            cargar_juego()
+            self.personaje.personaje = self.cargar_juego()
         else:
             print('Comando no disponible')
+
+    def guardar_juego(self):
+        try:
+            with open('./savegames/savegame.json','w') as f:
+                json.dump(self.personaje.personaje,f)
+                f.close()
+                print('Juego guardado')
+        except FileNotFoundError:
+            print('Falta el directorio savegame')
+
+    def cargar_juego(self):
+        try:
+            with open('./savegames/savegame.json') as f:
+                print('Juego Cargado')
+                #self.personaje.personaje = json.load(f)
+                return json.load(f)
+        except:
+            print('No esta el archivo de carga')
 
 #TODO: Agregar inventario (items tienen peso)
 class Personaje:
@@ -99,22 +100,33 @@ class Vertice:
         self.id = id
         self.aristas = []
         self.datos = obtener_informacion(self.id)
+        self.elementos = []
+        
 
     def insertar_arista(self,id):
         if id not in self.aristas:
             self.aristas.append(id)
 
+    def insertar_elemento(self, elemento):
+        self.elementos.append(elemento)
+    
     def informacion(self, propio = True):
         if propio:
             print('Tu ubicacion: '+self.id)
-        else:
-            print('Estas viendo: '+self.id)
-        print(self.datos['descripcion'])
-        if propio:
+            print(self.datos['descripcion'])
+            for indice in range(len(self.elementos)):
+                print('En el lugar encuentras '+self.elementos[indice].datos['nombre'])
             print('Posibles salidas: ')
             for arista in self.aristas:
                 print('->'+arista)
+        else:
+            print('Estas viendo: '+self.id)
+            print(self.datos['descripcion_lejana'])
 
+            
+class Elemento:
+    def __init__(self,nombre):
+        self.datos = obtener_informacion(nombre)
 
 if __name__ == "__main__":
     #TODO: hacer proceso de creacion del grafo
@@ -132,6 +144,8 @@ if __name__ == "__main__":
     for i in juego.mundo.vertices:
         print(i)
         print(juego.mundo.vertices[i].aristas)
+    mesa = Elemento('mesa')
+    juego.mundo.vertices['casa'].insertar_elemento(mesa)
     print('****Fin de Crear Mundo****')
     #TODO: proceso de creacion de personaje
     personaje = Personaje('Draculinio', 'casa')
